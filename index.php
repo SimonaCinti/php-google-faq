88924d52045e88b32f844f206aa16782fdc3d82d
Simona Cinti <simona.cinti2@gmail.com>
2020-12-16 16:45:44 +0100

    footer bar

--- index.php
+++ index.php
@@ -50,6 +50,8 @@
                 [
                     'question' => 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
                     'answer' => [
+                        "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+
                         "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
 
                         "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
@@ -82,17 +84,34 @@
             ];
             // var_dump($faq);
             foreach ($faqs as $faq) { ?>
+                <!-- Template for FAQ -->
                 <div class="faq-list">
                     <h3> <?php echo $faq["question"] ?></h3>
-                <?php foreach ($faq["answer"] as $answer_text) { ?>
+                    <?php foreach ($faq["answer"] as $answer_text) { ?>
                         <p> <?php echo $answer_text ?></p>
-                <?php } ?>
+                    <?php } ?>
                 </div>
             <?php } ?>
         </div>
     </main>
+
     <!-- Footer -->
     <footer>
+        <div class="container-footer">
+            <ul class="footer-list small">
+                <li>Google</li>
+                <li>Tutto su Google</li>
+                <li>Privacy</li>
+                <li>Termini</li>
+            </ul>
+            <div class="lang">
+                <i class="fas fa-globe"></i>
+                <select name="lang" id="lang">
+                    <option value="it">Italiano</option>
+                    <option value="en">English</option>
+                </select>
+            </div>
+        </div>
     </footer>
 </body>
 
