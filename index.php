4fe7ca54cb5facdc096e743ed0f5a4adc0612bff
Simona Cinti <simona.cinti2@gmail.com>
2020-12-16 14:38:34 +0100

    header nav

--- index.php
+++ index.php
@@ -1,15 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP google faq</title>
+    <!-- fontawesome -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css' />
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <!-- css -->
     <link rel="stylesheet" href="./dist/css/style.css">
 </head>
+
 <body>
-    <h1>Lorem</h1>
-    <h2>ipsum</h2>
-    <h3>hello</h3>
+    <!-- Header -->
+    <header>
+        <nav class="container">
+            <div class="title">
+                <img class="img-logo mr-15"src="./img/goglogo.png" alt="logo">
+                <p class="privacy">Privacy e Termini</p>
+            </div>
+            <div class="log">
+                <i class="fas fa-bars mr-15"></i>
+                <div class="btn">Accedi</div>
+            </div>
+        </nav>
+    </header>
+    <!-- Main -->
+    <main>
+    </main>
+    <!-- Footer -->
+    <footer>
+    </footer>
 </body>
+
 </html>
